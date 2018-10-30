02966aa313d8bbd9bf18807e174d9ece9e4e2050
tony <172993974@qq.com>
2018-10-30 16:34:30 +0800

    '添加评论'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -28,6 +28,23 @@ module.exports = {
                     buttonText: "刷新"
                 }
             }
+        ],
+        ['vuepress-plugin-comment', 
+            {
+                choosen: 'gitalk',
+                options: {
+                    clientID: '83ac3023b3f9cf5b5d0f',
+                    clientSecret: '05f091f622fe9071f9cf7f2cca27b5573e61b82b',
+                    repo: 'km',
+                    owner: 'fttony',
+                    admin: ['fttony'],
+                    id: '<%- frontmatter.commentid || frontmatter.permalink %>',
+                    distractionFreeMode: false,
+                    labels: ['Gitalk', 'Comment'],
+                    title: '「评论」<%- frontmatter.title %>',
+                    body: '<%- frontmatter.title %>：<%- window.location.origin %><%- frontmatter.to.path || window.location.pathname %>'
+                }
+            }
         ]
     ]
 }
